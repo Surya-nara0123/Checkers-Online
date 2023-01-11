258a3f96f7ddc52b0e0e2fe85c4b11a7ff2ccec0
Surya-nara0123 <surya.nara0123@gmail.com>
2023-01-11 21:05:49 +0530

    game functioning competely finished     ->bug fixes TODO: add server and cliet connections with database hosting

--- game.py
+++ game.py
@@ -6,7 +6,8 @@ import KingClass.functions
 class gameWindow:
     def __init__(self):
         self.height = self.width = 500
-        self.window = pyg.display.set_mode((self.width, self.height))
+        self.window = pyg.display.set_mode((self.width+220, self.height))
+        self.sidebar = pyg.transform.scale(pyg.image.load('Assets/sideScreen.png'), (200, self.height))
         self.boardImage = pyg.transform.flip(pyg.transform.scale(pyg.image.load("Assets/CheckersBoard.png"), (self.width, self.height)), False, True)
         self.run = True
         self.turn = 'bn'
@@ -40,6 +41,16 @@ class gameWindow:
                     else:
                         self.PeicesPositions[i].append('')
         #self.PeicesPositions[5][6] = 'wp'
+        #self.PeicesPositions = [
+        #    ['', '', '', '', '', '', '', 'wp'],
+        #    ['wp', '', '', '', '', '', '', ''],
+        #    ['', '', '', '', '', '', '', ''],
+        #    ['', '', 'wp', '', '', '', '', ''],
+        #    ['', '', '', '', '', '', '', 'bp'],
+        #    ['bp', '', '', '', '', '', 'wp', ''],
+        #    ['', '', '', '', '', 'bp', '', ''],
+        #    ['bp', '', '', '', '', '', 'bp', '']
+        #]
         for i in range(8):
             for j in range(8):
                 if self.PeicesPositions[i][j] != '':
@@ -100,7 +111,7 @@ class gameWindow:
                                 if self.PeicesPositions[i+1][j+1] == '':
                                     self.moves.append({'initial position':(i, j), 'final position':(i+1, j+1), 'type of move':'n'})
                         else:
-                            if j == 7 and i != 0:
+                            if j == 7:
                                 if i != 0:
                                     if self.PeicesPositions[i-1][j-1] == '':
                                         self.moves.append({'initial position':(i, j), 'final position':(i-1, j-1), 'type of move':'n'})
@@ -108,10 +119,12 @@ class gameWindow:
                                     if self.PeicesPositions[i+1][j-1] == '':
                                         self.moves.append({'initial position':(i, j), 'final position':(i+1, j-1), 'type of move':'n'})
                             else:
-                                if self.PeicesPositions[i-1][j+1] == '':
-                                    self.moves.append({'initial position':(i, j), 'final position':(i-1, j+1), 'type of move':'n'})
-                                if self.PeicesPositions[i+1][j+1] == '':
-                                    self.moves.append({'initial position':(i, j), 'final position':(i+1, j+1), 'type of move':'n'})
+                                if i != 0:
+                                    if self.PeicesPositions[i-1][j+1] == '':
+                                        self.moves.append({'initial position':(i, j), 'final position':(i-1, j+1), 'type of move':'n'})
+                                if i != 7:
+                                    if self.PeicesPositions[i+1][j+1] == '':
+                                        self.moves.append({'initial position':(i, j), 'final position':(i+1, j+1), 'type of move':'n'})
                         #condition of forced move
                         if j != 0 and j != 7:
                             if self.PeicesPositions[i-1][j-1] != '' and self.PeicesPositions[i-1][j-1][0] != 'b':
@@ -330,29 +343,35 @@ class gameWindow:
                     if i[0] == moveDict['initial position'][0] and i[1] == moveDict['initial position'][1]:
                         i[0], i[1] = moveDict['final position'][0], moveDict['final position'][1]
                         i[-1].spriteRect.center = self.boardPositions[moveDict['final position'][0]][moveDict['final position'][1]].center
+                        flag1 = False
+                        if moveDict['final position'][0] == 0 and (self.turn == 'wn' or self.turn[0] == 'b'):
+                            i[-1] = KingClass.functions.King('b', *i[-1].spriteRect.center)
+                            self.PeicesPositions[moveDict['final position'][0]][moveDict['final position'][1]] = 'bk'
+                            self.turn = 'wn'
+                            flag1 = True
+                        elif moveDict['final position'][0] == 7 and (self.turn == 'bn' or self.turn[0] == 'w'):
+                            i[-1] = KingClass.functions.King('w', *i[-1].spriteRect.center)
+                            self.PeicesPositions[moveDict['final position'][0]][moveDict['final position'][1]] = 'wk'
+                            self.turn = 'bn'
+                            flag1 = True
                         self.findMoves()
                         flag = False
                         for k in self.moves:
                             if k['type of move'] == 'f':
                                 flag = True
-                        if flag:
-                            if self.turn == 'wn':
-                                self.turn = f'wf {i[0]} {i[1]}'
+                        if not flag1:
+                            if flag:
+                                if self.turn[0] == 'w':
+                                    self.turn = f'wf {i[0]} {i[1]}'
+                                else:
+                                    self.turn = f'bf {i[0]} {i[1]}'
+                                    print('hello')
                             else:
-                                self.turn = f'bf {i[0]} {i[1]}'
-                        else:
-                            if self.turn == 'wn':
-                                self.turn = 'bn'
-                            else:
-                                self.turn = 'wn'
-                        if moveDict['final position'][0] == 0 and (self.turn == 'wn' or self.turn[0] == 'b'):
-                            i[-1] = KingClass.functions.King('b', *i[-1].spriteRect.center)
-                            self.PeicesPositions[moveDict['final position'][0]][moveDict['final position'][1]] = 'bk'
-                            self.turn = 'wn'
-                        elif moveDict['final position'][0] == 7 and (self.turn == 'bn' or self.turn[0] == 'w'):
-                            i[-1] = KingClass.functions.King('w', *i[-1].spriteRect.center)
-                            self.PeicesPositions[moveDict['final position'][0]][moveDict['final position'][1]] = 'wk'
-                            self.turn = 'bn'
+                                if self.turn == 'wn':
+                                    self.turn = 'bn'
+                                else:
+                                    self.turn = 'wn'
+                        
                     elif i[0] == (moveDict['initial position'][0]+moveDict['final position'][0])//2 and i[1] == (moveDict['initial position'][1]+moveDict['final position'][1])//2:
                         self.PeicesClass.pop(j)
                         j -= 1
@@ -362,6 +381,7 @@ class gameWindow:
     def main(self):
         while self.run:
             self.window.blit(self.boardImage, (0, 0))
+            self.window.blit(self.sidebar, (self.width+10, 0))
             for i in range(len(self.PeicesClass)):
                 self.window.blit(self.PeicesClass[i][-1].spriteImage, self.PeicesClass[i][-1].spriteRect)
                 self.findMoves()
@@ -386,12 +406,29 @@ class gameWindow:
                                         flag = True
                                     if (i, j) == m['final position'] and m['initial position'] == self.selected:
                                         if self.selected != (-1, 0):
-                                            self.move(m)
-                                            print(self.moves)
                                             print(*self.PeicesPositions,sep='\n')
+                                            self.move(m)
+                                            
                     if flag == False:
                         self.selected = (-1, 0)
-
+            c = 0
+            for i in self.PeicesPositions:
+                c += i.count('wp')
+                c += i.count('wk')
+            c1 = 0
+            for i in self.PeicesPositions:
+                c1 += i.count('bp')
+                c1 += i.count('bk')
+            if c1 == 0:
+                print('White Wins')
+                self.run = False
+                pyg.quit()
+                sys.exit()
+            if c == 0:
+                print('Black Wins')
+                self.run = False
+                pyg.quit()
+                sys.exit()
             pyg.display.update()
 
 gameWindow()
